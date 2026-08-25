2646339b14394a7b9dc2d2ca3cd640b65be05e32
RUBAGUMYA Mike Galen <hyllusx@gmail.com>
2026-08-25 20:17:47 +0200

    fix: parcels model migration fix

--- backend/app/models/parcel.py
+++ backend/app/models/parcel.py
@@ -1,7 +1,7 @@
 from uuid import UUID, uuid4
 
-# from geoalchemy2 import Geometry
-# from sqlalchemy import Float, String
+from geoalchemy2 import Geometry, WKBElement
+from sqlalchemy import Float, String
 from sqlalchemy.orm import Mapped, mapped_column
 
 from app.core.database import Base
@@ -14,24 +14,21 @@ class Parcel(Base):
         primary_key=True,
         default=uuid4,
     )
-
-    # upi: Mapped[str] = mapped_column(
-    #     String(100),
-    #     unique=True,
-    #     index=True,
-    #     nullable=False,
-    # )
-
-    # geometry: Mapped[object] = mapped_column(
-    #     Geometry(
-    #         geometry_type="POLYGON",
-    #         srid=4326,
-    #         spatial_index=True,
-    #     ),
-    #     nullable=False,
-    # )
-
-    # area_m2: Mapped[float | None] = mapped_column(
-    #     Float,
-    #     nullable=True,
-    # )
+    upi: Mapped[str] = mapped_column(
+        String(100),
+        unique=True,
+        index=True,
+        nullable=False,
+    )
+    geometry: Mapped[WKBElement] = mapped_column(
+        Geometry(
+            geometry_type="POLYGON",
+            srid=4326,
+            spatial_index=True,
+        ),
+        nullable=False,
+    )
+    area_m2: Mapped[float | None] = mapped_column(
+        Float,
+        nullable=True,
+    )
